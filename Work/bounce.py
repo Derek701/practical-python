85f88a832903544592b26f7fae7bde2b76c50766
Derek Miller <Derek701@users.noreply.github.com>
2020-06-10 21:59:54 -0500

    Complete Exercise 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
-#
+# 
 # Exercise 1.5
+
+height = 100
+rebound = 3/5
+bounces = 0
+
+while (bounces < 10):
+    height = round(height*rebound, 4)
+    bounces += 1
+    print(bounces, height)
