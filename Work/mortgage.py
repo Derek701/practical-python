0a01505b3b9206b4c9dce49d7af47d650a0cef65
Derek Miller <Derek701@users.noreply.github.com>
2020-06-10 23:41:19 -0500

    Complete Exercise 1.9

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,21 +1,24 @@
 # mortgage.py
 # 
-# Exercise 1.8
+# Exercise 1.9
 
 principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 num_payments = 0
+extra_payment_start_month = 60
+extra_payment_end_month = 108
+extra_payment = 1000
 
 while principal > 0:
     num_payments += 1
-    if num_payments == 1:
-        payment += 1000
-    elif num_payments == 13:
-        payment -= 1000
+    if num_payments == extra_payment_start_month:
+        payment += extra_payment
+    elif num_payments == extra_payment_end_month:
+        payment -= extra_payment
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
 
-print('Total paid', round(total_paid,2))
+print('Total paid', round(total_paid,2)) # Total paid 880074.1
 print('Months', num_payments)
