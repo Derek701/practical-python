df1c08f0da543bc6cb7efaeed55218bcbd66ebc0
Derek Miller <Derek701@users.noreply.github.com>
2020-06-16 16:24:51 -0500

    Complete Exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
 # pcost.py
-#
+# 
 # Exercise 1.27
+
+import os
+
+f = open('Data/portfolio.csv', 'rt')
+headers = next(f)
+cost = 0
+
+for line in f:
+    row = line.split(',')
+    cost += int(row[1]) * float(row[2])
+
+print('Total cost', cost)
+f.close()
