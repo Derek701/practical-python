70112565e0d47f98d6b48241c2b6b4d788c74d25
Derek Miller <Derek701@users.noreply.github.com>
2020-06-16 23:36:40 -0500

    Complete Exercise 2.5

--- Work/report.py
+++ Work/report.py
@@ -1,17 +1,17 @@
 # report.py
 # 
-# Exercise 2.4
+# Exercise 2.5
 
 import csv
 
 def read_portfolio(filename):
-    '''Opens a given portfolio file and reads it into a list of tuples'''
+    '''Opens a given portfolio file and reads it into a list of dictionaries'''
     portfolio = []
 
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
         for row in rows:
-            holding = (row[0], int(row[1]), float(row[2]))
+            holding = dict(name=row[0], shares=int(row[1]), price=float(row[2]))
             portfolio.append(holding)
     return portfolio
