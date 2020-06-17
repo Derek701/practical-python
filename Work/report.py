b3eb641e0dbc7f1994eb7d06ada6294cd1b6fc8b
Derek Miller <Derek701@users.noreply.github.com>
2020-06-17 00:18:36 -0500

    Complete Exercise 2.6

--- Work/report.py
+++ Work/report.py
@@ -1,6 +1,6 @@
 # report.py
 # 
-# Exercise 2.5
+# Exercise 2.6
 
 import csv
 
@@ -15,3 +15,16 @@ def read_portfolio(filename):
             holding = dict(name=row[0], shares=int(row[1]), price=float(row[2]))
             portfolio.append(holding)
     return portfolio
+
+def read_prices(filename):
+    '''Reads a set of prices into a dict where keys are stock names and values are stock prices'''
+    price = {}
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                price[row[0]] = float(row[1])
+            except IndexError:
+                print("Couldn't parse", row)
+    return price
