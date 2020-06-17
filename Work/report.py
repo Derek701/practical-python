cfd13ad52439377beea0b297741de86a4ad3f390
Derek Miller <Derek701@users.noreply.github.com>
2020-06-16 23:11:58 -0500

    Complete Exercise 2.4

--- Work/report.py
+++ Work/report.py
@@ -4,15 +4,14 @@
 
 import csv
 
-def portfolio_cost(filename):
-    '''Computes the total cost (shares*price) of a portfolio file'''
-    total_cost = 0.0
+def read_portfolio(filename):
+    '''Opens a given portfolio file and reads it into a list of tuples'''
+    portfolio = []
 
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
         for row in rows:
-            nshares = int(row[1])
-            price = float(row[2])
-            total_cost += nshares * price
-    return total_cost
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+    return portfolio
