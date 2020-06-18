31a354af7482fc0b2070e771a19f2bb678bdd986
Derek Miller <Derek701@users.noreply.github.com>
2020-06-18 11:13:30 -0500

    Complete Exercise 3.1

--- Work/report.py
+++ Work/report.py
@@ -1,14 +1,16 @@
 # report.py
 # 
-# Exercise 2.24
+# Exercise 3.1
 
 import csv
 
 def read_portfolio(filename):
-    '''Opens a given portfolio file and reads it into a list of dictionaries'''
+    '''
+    Read a stock portfolio file into a list of dictionaries with keys
+    name, shares, and price.
+    '''
     portfolio = []
-
-    with open(filename, 'rt') as f:
+    with open(filename) as f:
         rows = csv.reader(f)
         headers = next(rows)
         select = ['name', 'shares', 'price']
@@ -18,10 +20,11 @@ def read_portfolio(filename):
     return portfolio
 
 def read_prices(filename):
-    '''Reads a set of prices into a dict where keys are stock names and values are stock prices'''
+    '''
+    Read a CSV file of price data into a dict mapping names to prices
+    '''
     prices = {}
-
-    with open(filename, 'rt') as f:
+    with open(filename) as f:
         rows = csv.reader(f)
         for row in rows:
             try:
@@ -33,25 +36,16 @@ def read_prices(filename):
 def make_report(stocks, prices):
     ''' Takes a list of stocks and dict of prices and returns a list of tuples with report data'''
     report = []
-
     for s in stocks:
         holding = (s['name'], s['shares'], prices[s['name']], prices[s['name']]-s['price'])
         report.append(holding)
     return report
 
-portfolio = read_portfolio('Data/portfolio.csv')
-prices = read_prices('Data/prices.csv')
-report = make_report(portfolio, prices)
-headers = ('Name', 'Shares', 'Price', 'Change')
+def print_report(report):
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
+    print(f'{"":->10s} {"":->10s} {"":->10s} {"":->10s}')
+    for name, shares, price, change in report:
+        print(f'{name:>10s} {shares:>10d} {"$"+str(price):>10s} {change:>10.2f}')
 
-cost = sum([ s['shares'] * s['price'] for s in portfolio ])         # Total cost 44671.15
-value = sum([ s['shares'] * prices[s['name']] for s in portfolio ]) # Current value 28686.1
-gain = value - cost                                                 # Gain/loss -15985.05
-
-header = f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}'
-separator = f'{"":->10s} {"":->10s} {"":->10s} {"":->10s}'
-
-print(header)
-print(separator)
-for name, shares, price, change in report:
-    print(f'{name:>10s} {shares:>10d} {"$"+str(price):>10s} {change:>10.2f}')
+print_report(make_report(read_portfolio('Data/portfolio.csv'), read_prices('Data/prices.csv')))
