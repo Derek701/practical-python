9bc1b9b03d75768458a671f114d0cc6b3606aa7c
Derek Miller <Derek701@users.noreply.github.com>
2020-06-18 11:26:46 -0500

    Complete Exercise 3.2

--- Work/report.py
+++ Work/report.py
@@ -1,6 +1,6 @@
 # report.py
 # 
-# Exercise 3.1
+# Exercise 3.2
 
 import csv
 
@@ -33,19 +33,35 @@ def read_prices(filename):
                 continue # Skip empty line without printing error message
     return prices
 
-def make_report(stocks, prices):
-    ''' Takes a list of stocks and dict of prices and returns a list of tuples with report data'''
+def make_report(portfolio, prices):
+    '''
+    Make a list of (name, shares, price, change) tuples given a portfolio list
+    and prices dictionary.
+    '''
     report = []
-    for s in stocks:
+    for s in portfolio:
         holding = (s['name'], s['shares'], prices[s['name']], prices[s['name']]-s['price'])
         report.append(holding)
     return report
 
 def print_report(report):
+    '''
+    Print a nicely formated table from a list of (name, shares, price, change) tuples.
+    '''
     headers = ('Name', 'Shares', 'Price', 'Change')
     print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
     print(f'{"":->10s} {"":->10s} {"":->10s} {"":->10s}')
     for name, shares, price, change in report:
         print(f'{name:>10s} {shares:>10d} {"$"+str(price):>10s} {change:>10.2f}')
 
-print_report(make_report(read_portfolio('Data/portfolio.csv'), read_prices('Data/prices.csv')))
+def portfolio_report(portfolio_filename, prices_filename):
+    '''
+    Make a stock report given portfolio and price data files.
+    '''
+    portfolio = read_portfolio(portfolio_filename)
+    prices = read_prices(prices_filename)
+    report = make_report(portfolio, prices)
+
+    print_report(report)
+
+portfolio_report('Data/portfolio.csv', 'Data/prices.csv')
