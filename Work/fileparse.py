565f84656f57c4e7f948799e20ce95035a0726d0
Derek Miller <Derek701@users.noreply.github.com>
2020-06-22 09:27:06 -0500

    Complete Exercise 3.6

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,41 +1,45 @@
 # fileparse.py
 # 
-# Exercise 3.5
+# Exercise 3.6
 
 import csv
 
-def parse_csv(filename, select=None, types=None):
+def parse_csv(filename, select=None, types=None, has_headers=True):
     '''
     Parse a CSV file into a list of records with type conversion.
     '''
     with open(filename) as f:
         rows = csv.reader(f)
 
-        # Read the file headers
-        headers = next(rows)
-
-        # If a column selector was given, find indices of the specified columns.
-        # Also narrow the set of headers used for resulting dictionaries
-        if select:
-            indices = [headers.index(colname) for colname in select]
-            headers = select
+        # Read the file headers (if any)
+        if has_headers:
+            headers = next(rows)
         else:
-            indices = []
+            headers = []
+
+        # If specific columns have been selected, make indices for filtering
+        if select:
+            indices = [ headers.index(colname) for colname in select ]
+            headers = select
 
         records = []
         for row in rows:
             if not row:    # Skip rows with no data
                 continue
-            # Filter the row if specific columns were selected
-            if indices:
+
+            # If specific column indices are selected, pick them out
+            if select:
                 row = [ row[index] for index in indices ]
 
             # Apply type conversion to the row
             if types:
-                row = [func(val) for func, val in zip(types, row) ]
+                row = [ func(val) for func, val in zip(types, row) ]
 
-            # Make a dictionary
-            record = dict(zip(headers, row))
+            # Make a dictionary or a tuple
+            if headers:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
             records.append(record)
 
     return records
